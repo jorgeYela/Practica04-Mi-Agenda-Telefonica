4d76c7d6099a6e532de646a2020e3e34e64d6943
jorgeYela <yelajorge6@gmail.com>
2020-05-30 13:02:28 -0600

    Updated

--- config/conexioBD.php
+++ config/conexioBD.php
@@ -13,11 +13,7 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }else{
-<<<<<<< HEAD
-        
-=======
-    
->>>>>>> origin/Isaac-Chavez
+
     }
 
 
