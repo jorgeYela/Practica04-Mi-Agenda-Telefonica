a0ee215cb167e22e6d1929b45dd781b95ec2dd4d
jorgeYela <yelajorge6@gmail.com>
2020-05-31 16:34:41 -0600

    Upgrated

--- public/controladores/login.php
+++ public/controladores/login.php
@@ -4,8 +4,8 @@
 
     include '../../config/conexioBD.php';
 
-    $usuario = isset($_POST["usuario69"]) ? trim($_POST["usuario69"]) : null;
-    $contrasena = isset($_POST["contrasena69"]) ? trim($_POST["contrasena69"]) : null;
+    $usuario = isset($_POST["correo"]) ? trim($_POST["correo"]) : null;
+    $contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]) : null;
 
     $sql = "SELECT * FROM usuario WHERE usu_correo = '$usuario' and usu_contrasena = MD5('$contrasena')";
 
@@ -14,12 +14,10 @@
     if($result->num_rows > 0) {
 
         $_SESSION['isLogged'] = TRUE;
-        echo "<p>Muy Bien</p>";
         header("Location: ../../admin/vista/usuario/index.php");
 
     } else {
 
-        echo "<p>Muy Mal</p>";
         header("Location: ../vista/login.html");
 
     }
